701e155509cf70420ec9a66538914fad93e183fd
paulrwade <pau2237029@maricopa.edu>
2025-04-30 21:46:12 -0700

    Fix: enable CORS on all API routes to support frontend integration

--- explicable-ui/src/App.js
+++ explicable-ui/src/App.js
@@ -1,21 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fetchUsers = () => {
+    fetch('https://kym19kis0l.execute-api.us-east-2.amazonaws.com/dev/users', {
+      headers: {
+        'x-api-key': 'TNqG6DHCUk2AM1FkiLUGt8V2XpBq6Llw7d19MPIB'
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log('Users:', data))
+      .catch(err => console.error('Error:', err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Explicable</h1>
+        <p>React frontend successfully initialized. Ready to connect to the API!</p>
+
+        <button onClick={fetchUsers}>
+          Fetch Users from API
+        </button>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/prw760/explicable"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View Monorepo on GitHub
         </a>
       </header>
     </div>
