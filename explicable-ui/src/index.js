ab8b47a867998e47bd1aacad955d5e7efa446b00
paulrwade <pau2237029@maricopa.edu>
2025-05-02 08:26:59 -0700

    WORKING VERSION (FINALLY) OF LOGIN BUTTON AND SIGN-OUT BUTTON

--- explicable-ui/src/index.js
+++ explicable-ui/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_TKawr4zed",
+  client_id: "58br895ro7pi2ho11j69d9mqkg",
+  redirect_uri: "http://localhost:3000",
+  response_type: "code",
+  scope: "email openid phone profile",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
