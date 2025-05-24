7545cec130e0cd3971b88f44b6292654d1fb02c8
paulrwade <pau2237029@maricopa.edu>
2025-05-23 18:27:25 -0700

    test(e2e): scaffold Cypress and verify test runner integration

--- explicable-ui/cypress/e2e/login-flow.cy.js
+++ explicable-ui/cypress/e2e/login-flow.cy.js
@@ -1,8 +1,21 @@
-// cypress/e2e/login-flow.cy.js
+// explicable-ui/cypress/e2e/login-flow.cy.js
 
-describe('Login Flow', () => {
-  it('displays login button on landing page', () => {
-    cy.visit('/');
-    cy.contains('Create Account or Log In').should('exist');
+describe("Login Flow", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("displays login button without triggering real auth", () => {
+    cy.contains("Create Account or Log In")
+      .should("exist")
+      .then(($btn) => {
+        // Prevent redirect by stubbing window.location.assign
+        cy.stub($btn[0].ownerDocument.defaultView, "location", {
+          assign: cy.stub().as("locationAssign")
+        });
+      })
+      .click();
+
+    cy.get("@locationAssign").should("have.been.called");
   });
 });
